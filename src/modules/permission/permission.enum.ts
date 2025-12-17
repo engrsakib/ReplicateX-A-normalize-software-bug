ad28bb815eb27bbb3cf4c8d4a2e20a972fd28d97
engrsakib <snazmussakib01@gmail.com>
2025-12-17 17:41:11 +0600

    customer permision update

--- src/modules/permission/permission.enum.ts
+++ src/modules/permission/permission.enum.ts
@@ -4,6 +4,7 @@ export enum PermissionEnum {
   ORDER_UPDATE = "order.update",
   ORDER_VIEW = "order.view",
   ORDER_REPORT_VIEW = "order.report.view",
+  CUSTOMER_ORDER_REPORT_VIEW = "customer.order.report.view",
   ORDER_SUPER_UPDATE = "order.super.update",
 
   PRODUCT_CREATE = "product.create",
