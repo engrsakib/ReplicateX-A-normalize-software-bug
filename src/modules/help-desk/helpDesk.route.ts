08cbfbbea8c1f56dfb09f1a4fb80383567027537
engrsakib <snazmussakib01@gmail.com>
2026-01-23 02:13:58 +0600

    update ddaa

--- src/modules/help-desk/helpDesk.route.ts
+++ src/modules/help-desk/helpDesk.route.ts
@@ -2,14 +2,12 @@ import { ROLES } from "@/constants/roles";
 import { JwtInstance } from "@/lib/jwt";
 import { Router } from "express";
 import { HelpDeskController } from "./helpDesk.controller";
-import validateRequest from "@/middlewares/validateRequest";
-import { helpDesk_validation } from "./helpDesk.validation";
 
 const router = Router();
 
 router.post(
   "/",
-  validateRequest(helpDesk_validation.createPostValidationSchema),
+
   JwtInstance.authenticate(Object.values(ROLES)),
   HelpDeskController.create
 );
@@ -22,7 +20,7 @@ router.patch(
 
 router.patch(
   "/:id",
-  validateRequest(helpDesk_validation.updatePostValidationSchema),
+
   JwtInstance.authenticate(Object.values(ROLES)),
   HelpDeskController.update
 );
